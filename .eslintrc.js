69f58d4e046f17d9412de3b034d47f53d5b1f0d0
Jamie Connolly <jamie.connolly@tailify.com>
2018-02-26 15:13:22 +0000

    Add in some basic tests using Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,26 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    'jest/globals': true,
+    'node': true,
   },
   extends: [
     'eslint:recommended',
+    'plugin:jest/recommended',
     'plugin:node/recommended',
   ],
+  parserOptions: {
+    sourceType: 'module',
+  },
   plugins: [
+    'jest',
     'node',
   ],
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'indent': ['error', 2],
     'linebreak-style': 'error',
-    'quote-props': ['error', 'as-needed'],
+    'quote-props': ['error', 'consistent-as-needed'],
     'quotes': ['error', 'single', { avoidEscape: true }],
     'semi': 'error',
     'sort-keys': 'error',
