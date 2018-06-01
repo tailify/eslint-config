90b89d5a1b203e708e509678a418f9650f9bdd50
Jamie Connolly <jamie.connolly@tailify.com>
2018-06-01 08:58:05 +0100

    Restrict JSX to only being contained in .js files

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: 'airbnb',
+  rules: {
+    // restrict file extensions that may contain JSX
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+  },
 };
