65f3d98df50e9247e60fcaac45fc82f1c1d5e3b8
Jamie Connolly <jamie.connolly@tailify.com>
2018-11-18 16:33:26 +0000

    Remove duplicated rules

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb',
-    './rules/best-practices',
     './rules/import',
     './rules/react',
     './rules/react-a11y',
-    './rules/stylistic',
   ].map(require.resolve),
 };
--- rules/react.js
+++ rules/react.js
@@ -2,24 +2,6 @@
 
 module.exports = {
   rules: {
-    // enforce that class methods utilize `this`
-    'class-methods-use-this': ['error', {
-      exceptMethods: [
-        'render',
-        'getInitialState',
-        'getDefaultProps',
-        'getChildContext',
-        'componentWillMount',
-        'componentDidMount',
-        'componentWillReceiveProps',
-        'shouldComponentUpdate',
-        'componentWillUpdate',
-        'componentDidUpdate',
-        'componentWillUnmount',
-        'componentDidCatch',
-      ],
-    }],
-
     // enforces consistent naming for boolean props
     'react/boolean-prop-naming': ['error', {
       propTypeNames: ['bool', 'mutuallyExclusiveTrueProps'],
@@ -30,13 +12,6 @@ module.exports = {
 
     // forbid foreign `propTypes`
     'react/forbid-foreign-prop-types': ['error', { allowInPropTypes: true }],
-
-    // forbid certain `propTypes`
-    'react/forbid-prop-types': ['error', {
-      checkChildContextTypes: true,
-      checkContextTypes: true,
-      forbid: ['any', 'array', 'object'],
-    }],
 
     // restrict file extensions that may contain JSX
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.mjs'] }],
@@ -57,30 +32,36 @@ module.exports = {
       shorthandLast: false,
     }],
 
-    // validate whitespace in and around the JSX opening and closing brackets
-    'react/jsx-tag-spacing': 'error',
-
     // prevent direct mutation of `this.state`
     'react/no-direct-mutation-state': 'error',
 
     // enforce component methods order
     'react/sort-comp': ['error', {
+      groups: {
+        rendering: [
+          '/^render.+$/',
+          'render',
+        ],
+      },
       order: [
         'static-methods',
+        'instance-variables',
         'lifecycle',
         '/^(handle|on).+$/',
         'getters',
         'setters',
         '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+        'instance-methods',
         'everything-else',
-        '/^render.+$/',
-        'render',
+        'rendering',
       ],
     }],
 
     // enforce propTypes declarations alphabetical sorting
     'react/sort-prop-types': ['error', {
+      callbacksLast: false,
       ignoreCase: true,
+      requiredFirst: false,
       sortShapeProp: true,
     }],
   },
